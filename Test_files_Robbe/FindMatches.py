e4a90daa3b2b2bcab319eb87fa4cc922f38ad61d
rovermei <Robbe.Vermeire@UGent.be>
2020-11-22 19:06:26 +0100

    Added findmatches test

---
+++ Test_files_Robbe/FindMatches.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+import math
+import os
+
+dirpath = os.path.dirname(os.path.abspath(__file__))
+
+
+imagelist= []
+for i in range(1,8):
+    imagelist.append(cv2.imread(dirpath+"\images\\QueryImages\\" + str(i) + ".PNG"))
+    
+queryimage = cv2.imread(dirpath+"\\images\\QueryImages\\1.PNG",0)
+
+
+orb = cv2.ORB_create()
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
+orblist =[]
+matcheslist = []
+
+# queryimage = cv2.Canny(queryimage,100,200)
+kp2, des2 = orb.detectAndCompute(queryimage, None)
+
+for image in imagelist:
+    # image = cv2.Canny(image,100,200)
+    kp, des = orb.detectAndCompute(image, None)
+    orblist.append((kp,des))
+    
+    matches = bf.match(des, des2)
+    matches = sorted(matches, key = lambda x:x.distance)
+    matcheslist.append(matches)
+    
+    matches_img = cv2.drawMatches(image,kp,queryimage,kp2,matches, outImg=None, flags=2)
+    print(len(matches))
+    plt.imshow(matches_img)
+    plt.show()
+
+
+
